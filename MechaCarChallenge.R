fc51aa630b581e99431dab0586cf2df738a7c310
Pritpal Sidhu <pritpalsidhu@pritpals-mbp.lan>
2022-07-03 13:00:05 -0400

    Updated MechaCarChallenge.R file

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -12,6 +12,7 @@ lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,data=Mec
 # Summarize Liner model
 summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,data=MechaCarMPG))
 
+
 ### Deliverable 2
 
 # Load csv file as data frame.
@@ -23,3 +24,16 @@ summarize_coil <- CoilData %>% summarize(Mean=mean(PSI), Median=median(PSI), Var
 # Create Lot Summary
 lot_coil_summary <- CoilData %>% group_by(Manufacturing_Lot) %>% 
   summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
+
+
+### Deliverable 3
+
+# T-test on All Coils
+t.test((CoilData$PSI), mu=1500)
+
+# T-test on each Lot
+t.test(subset(CoilData, Manufacturing_Lot=="Lot1")$PSI, mu=1500)
+t.test(subset(CoilData, Manufacturing_Lot=="Lot2")$PSI, mu=1500)
+t.test(subset(CoilData, Manufacturing_Lot=="Lot3")$PSI, mu=1500)
+
+
